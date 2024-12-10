470873a35d13ffaee897c9d297e8e86ba552d312
annyasebastian <94396859+annyasebastian@users.noreply.github.com>
2024-12-10 21:38:29 +0100

    Update find_prime.py

--- find_prime.py
+++ find_prime.py
@@ -3,9 +3,10 @@
 
 def allPrimesUpTo(num):                       # eg : num = 100
     primesFound = [2]                         # 2 is already included
-    for n in range(3, num):                   # for n in 3 to 99: 
+    for n in range(3, num): 
+        limit = int(n ** 0.5) + 1             # for n in 3 to 99: 
         for factor in primesFound:            #   for factor in [2,...]
-            if factor < int(n ** 0.5) + 1:    #      if factor < 11
+            if factor < limit:                #      if factor < 11
                 if n % factor == 0:
                     break
         else:
