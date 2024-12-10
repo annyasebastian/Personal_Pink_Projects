9ec3358070269f1e16811a63711d8bb90558b00c
annyasebastian <94396859+annyasebastian@users.noreply.github.com>
2024-12-10 21:27:20 +0100

    Create find_prime.py

---
+++ find_prime.py
@@ -0,0 +1,13 @@
+# Fastest way to find prime numbers
+# a function that returns prime numbers till num
+
+def allPrimesUpTo(num):                       # eg : num = 100
+    primesFound = [2]                         # 2 is already included
+    for n in range(3, num):                   # for n in 3 to 99: 
+        for factor in primesFound:            #   for factor in [2,...]
+            if factor < int(n ** 0.5) + 1:    #      if factor < 11
+                if n % factor == 0:
+                    break
+        else:
+            primesFound.append(n)
+    return primesFound
