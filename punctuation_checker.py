c6efb5ad7bd10227e7ef740915d87c7df1c226e7
annyasebastian <94396859+annyasebastian@users.noreply.github.com>
2024-12-21 03:18:07 +0100

    Update punctuation_checker.py

--- punctuation_checker.py
+++ punctuation_checker.py
@@ -5,4 +5,4 @@ print(f"Punctuations are any of the following: {string.punctuation}")
 def contains_punctuation(input_str):
     ''' Return True if the input_str contains punctuations.
     Return False otherwise'''
-    return any(input_str[i] in string.punctuation for i in range(0,len(input_str)))
+    return any(char in string.punctuation for char in input_str)
